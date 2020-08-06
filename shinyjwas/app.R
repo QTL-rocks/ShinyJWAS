b83bdfd40778556aee4031571e196362deaa7841
Zigui Wang <ziguiwang@Ziguis-MacBook-Pro.local>
2020-08-06 07:49:47 -0700

    update for small bugs

---
+++ shinyjwas/app.R
@@ -0,0 +1,14 @@
+# Define UI ----
+#rm(list=ls())
+# Load required packages
+#library(shiny)
+#library(shinyFiles)
+#library(rclipboard)
+#library(ggplot2)
+#library(coda)
+
+
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
+
